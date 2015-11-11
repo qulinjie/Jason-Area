16dc951bfeda3dd90ca089da29a87a05bdbce0b7
zhangkui <zhangkui@d4452c07-b0bf-42be-a67f-5257330459db>
2015-11-11 08:20:14 +0000

    修改返回格式。

--- ddmg_pay/branches/ddmg_pay/services/Callback.php
+++ ddmg_pay/branches/ddmg_pay/services/Callback.php
@@ -6,7 +6,8 @@ class Callback
     public function __construct()
     {}
 
-    public function request($param)
+    /*
+    public function request2($param)
     {
         $logFileName = 'K:/' . date('Y-m-d') . '.log';
         $myfile = @fopen($logFileName, "w");
@@ -17,6 +18,34 @@ class Callback
         );
         $result['date'] = date('Y-m-d H:i:s',time());
         $result = json_encode($result);
+        fwrite($myfile, date('Y-m-d H:i:s',time()) . " response=##" . $result . "##\r\n");
+        fclose($myfile);
+        return $result;
+    }
+    */
+    
+    public function request($param)
+    {
+        //$logFileName = 'K:/' . date('Y-m-d') . '.log';
+		$logFileName = '/alidata/www/ddmg_pay/log/' . date('Y-m-d') . '.log';
+		
+        $myfile = @fopen($logFileName, "w");
+        date_default_timezone_set(PRC);
+        fwrite($myfile, date('Y-m-d H:i:s',time()) . " request=##" . $param . "##\r\n");
+        
+        $result = "";
+        $result .= "<Service>";
+        $result .= "	<Header>";
+        $result .= "		<RequestTime>" . date('Y-m-d H:i:s',time()) . "</RequestTime>";
+        $result .= "	</Header>";
+        $result .= "	<Body>";
+        $result .= "		<Response>";
+        $result .= "			<RESULT>SUCCESS</RESULT>";
+        $result .= "			<MSG>成功</MSG>";
+        $result .= "		</Response>";
+        $result .= "	</Body>";
+        $result .= "</Service>";
+        
         fwrite($myfile, date('Y-m-d H:i:s',time()) . " response=##" . $result . "##\r\n");
         fclose($myfile);
         return $result;
