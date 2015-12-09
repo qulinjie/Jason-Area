cb108c32e540433d5eb2b4cbb5056a0225564d21
zhangkui <zhangkui@d4452c07-b0bf-42be-a67f-5257330459db>
2015-12-09 02:10:10 +0000

    开发授权码管理功能。增加、停用。

--- ddmg_pay/trunk/controller/AuthorizationCodeController.class.php
+++ ddmg_pay/trunk/controller/AuthorizationCodeController.class.php
@@ -19,6 +19,21 @@ class AuthorizationCodeController extends Controller {
                 case 'searchList':
                     $this->searchList();
                     break;
+                case 'changeStatus':
+                    $this->changeStatus();
+                    break;
+                case 'delete':
+                    $this->delete();
+                    break;
+                case 'create':
+                    $this->create();
+                    break;
+                case 'getCode':
+                    $this->getCode();
+                    break;
+                case 'checkCode':
+                    $this->checkCode();
+                    break;
                 default:
                     Log::error('page not found . ' . $params[0]);
                     EC::fail(EC_MTD_NON);
@@ -30,17 +45,17 @@ class AuthorizationCodeController extends Controller {
     protected function searchList($isIndex = false) {
         $current_page = Request::post('page');
         $code = Request::post('code');
-        
-        Log::notice('searchList--------------------->>> page=' . $current_page);
-        if(!$current_page) {
-            $current_page = 1;
-        }
+        $time1 = Request::post('time1');
+        $time2 = Request::post('time2');
+        $type = Request::post('type');
+        $status = Request::post('status');
         
         $code_model = $this->model('authorizationCode');
-        $params  = array();
+        $user_id = AuthorizationCodeController::getCurrentUserId();
         
-        if($code) {
-            $params['code'] = $code;
+        $params  = array();
+        foreach ([ 'user_id', 'code', 'time1', 'time2', 'type', 'status'] as $val){
+            if($$val) $params[$val] = $$val;
         }
         
         $data_cnt = $code_model->searchCnt($params);
@@ -56,10 +71,10 @@ class AuthorizationCodeController extends Controller {
         
         $total_page = ($cnt % $page_cnt) ? (integer)($cnt / $page_cnt) + 1 : $cnt / $page_cnt;
         
-        if($current_page > $total_page) {
-            $current_page = $total_page;
-        } else if( 0 >= $current_page){
+        if(!$current_page || 0 >= $current_page) {
             $current_page = 1;
+        } if($current_page > $total_page) {
+            $current_page = $total_page;
         }
         
         $params['current_page'] = $current_page;
@@ -70,7 +85,6 @@ class AuthorizationCodeController extends Controller {
             EC::fail($data['code']);
         }
         
-        Log::notice("=====================>>> current_page=" . $current_page . ',total_page=' . $total_page );
         $data_list = $data['data'];
         $entity_list_html = $this->render('authorizationCode_list', array('data_list' => $data_list, 'current_page' => $current_page, 'total_page' => $total_page), true);
         if($isIndex) {
@@ -79,6 +93,256 @@ class AuthorizationCodeController extends Controller {
         } else {
             EC::success(EC_OK, array('entity_list_html' => $entity_list_html));
         }
+    }
+ 
+    private function changeStatus(){
+        $id = Request::post('id');
+        $status = Request::post('status');
+        
+        Log::notice('changeStatus-Request ==== >>> id=' . $id . ',status=' . $status);
+        if(!$id || !$status){
+            Log::error('changeStatus params error!');
+            EC::fail(EC_PAR_ERR);
+        }
+    
+        $code_model = $this->model('authorizationCode');
+        $user_id = AuthorizationCodeController::getCurrentUserId();
+    
+        $params = array();
+        $params['id'] = $id;
+        $params['user_id'] = $user_id;
+        
+        if(empty($params)){
+            Log::error('update params is empty!');
+            EC::fail(EC_PAR_BAD);
+        }
+        
+        $data_old = $code_model->getInfo($params);
+        if(EC_OK != $data_old['code']){
+            Log::error('getInfo Fail!');
+            EC::fail($data_old['code']);
+        }
+        $data_obj = $data_old['data'];
+        if(empty($data_obj)) {
+            Log::error('getInfo empty !');
+            EC::fail(EC_RED_EMP);
+        }
+        if( AuthorizationCodeModel::$_is_delete_true == $data_obj['is_delete'] ) {
+            Log::error('record had delete . is_delete=' . $data_obj['is_delete']);
+            EC::fail(EC_RED_EXP);
+        }
+        if( AuthorizationCodeModel::$_status_overdue == $data_obj['status'] || $status == $data_obj['status'] ) {
+            Log::error('record status is exception . status=' . $data_obj['status']);
+            EC::fail(EC_RED_EXP);
+        }
+        
+        $params['status'] = ($status == AuthorizationCodeModel::$_status_disabled) ? AuthorizationCodeModel::$_status_enabled : AuthorizationCodeModel::$_status_disabled;
+        $params['disenabled_timestamp'] = ($status == AuthorizationCodeModel::$_status_disabled) ? '0000-00-00 00:00:00' : date('Y-m-d H:i:s',time()) ;
+        
+        Log::notice('changeStatus ==== >>> params=' . json_encode($params) );
+        $data = $code_model->update($params);
+        if(EC_OK != $data['code']){
+            Log::error('update Fail!');
+            EC::fail($data['code']);
+        }
+        EC::success(EC_OK);
+    }
+    
+    private function delete(){
+        $id = Request::post('id');
+    
+        if(!$id){
+            Log::error('delete params error!');
+            EC::fail(EC_PAR_ERR);
+        }
+    
+        $code_model = $this->model('authorizationCode');
+        $user_id = AuthorizationCodeController::getCurrentUserId();
+    
+        $params = array();
+        $params['id'] = $id;
+        $params['user_id'] = $user_id;
+    
+        if(empty($params)){
+            Log::error('delete params is empty!');
+            EC::fail(EC_PAR_BAD);
+        }
+    
+        $data_old = $code_model->getInfo($params);
+        if(EC_OK != $data_old['code']){
+            Log::error('getInfo Fail!');
+            EC::fail($data_old['code']);
+        }
+        $data_obj = $data_old['data'];
+        if(empty($data_obj)) {
+            Log::error('getInfo empty !');
+            EC::fail(EC_RED_EMP);
+        }
+        if( AuthorizationCodeModel::$_is_delete_true == $data_obj['is_delete'] ) {
+            Log::error('record had delete . is_delete=' . $data_obj['is_delete']);
+            EC::fail(EC_RED_EXP);
+        }
+        if( 0 < $data_obj['used_count'] ) {
+            Log::error('record is exception . used_count=' . $data_obj['used_count']);
+            EC::fail(EC_RED_EXP);
+        }
+        
+        $params['is_delete'] = AuthorizationCodeModel::$_is_delete_true;
+    
+        Log::notice('changeStatus-delete ==== >>> params=' . json_encode($params) );
+        $data = $code_model->update($params);
+        if(EC_OK != $data['code']){
+            Log::error('update Fail!');
+            EC::fail($data['code']);
+        }
+        EC::success(EC_OK);
+    }
+    
+    private function create(){
+        $code = Request::post('code');
+        $type = Request::post('type');
+        $active_count = Request::post('active_count');
+        $time_start = Request::post('time_start');
+        $time_end = Request::post('time_end');
+        $comment = Request::post('comment');
+    
+        if(!$code || !$type){
+            Log::error('create params error!');
+            EC::fail(EC_PAR_ERR);
+        }
+        
+        $code_model = $this->model('authorizationCode');
+        $user_id = AuthorizationCodeController::getCurrentUserId();
+    
+        $data_old = $code_model->getInfo(array('code' => $code));
+        if(EC_OK != $data_old['code']){
+            Log::error('getInfo Fail!');
+            EC::fail($data_old['code']);
+        }
+        $data_obj = $data_old['data'];
+        if(!empty($data_obj)) {
+            Log::error('getInfo code is exists ! id=' . $data_obj['id']);
+            EC::fail(EC_RED_EMP);
+        }
+    
+        $params = array();
+        $params['user_id'] = $user_id;
+        $params['code'] = $code;
+        $params['type'] = $type;
+        $params['comment'] = $comment;
+        
+        if(AuthorizationCodeModel::$_type_count == $type){
+            $params['active_count'] = $active_count;
+            $params['time_start'] = AuthorizationCodeModel::$_empyt_time;
+            $params['time_end'] = AuthorizationCodeModel::$_empyt_time;
+        } else if(AuthorizationCodeModel::$_type_time == $type){
+            $params['active_count'] = 0;
+            $params['time_start'] = $time_start;
+            $params['time_end'] = $time_end;
+        } else {
+            Log::error('create params error! type=' . $type);
+            EC::fail(EC_PAR_ERR);
+        }
+        
+        if(empty($params)){
+            Log::error('create params is empty!');
+            EC::fail(EC_PAR_BAD);
+        }
+    
+        $data = $code_model->create($params);
+        if(EC_OK != $data['code']){
+            Log::error('create Fail!');
+            EC::fail($data['code']);
+        }
+        EC::success(EC_OK);
+    }
+    
+    private function getCode(){
+        $authCode = '';
+        while(true) {
+            $code_model = $this->model('authorizationCode');
+            $authCode = $code_model->getRandChar(4) . rand(999,9999); //授权码生成规则：4字符+4个数字
+            $data_authCode = $code_model->getInfo(array('code' => $authCode));
+            if(EC_OK != $data_authCode['code']){
+                Log::error('getInfo failed . code=' . $authCode);
+                EC::fail($data_authCode['code']);
+            }
+            if ( empty($data_authCode['data']) ){
+                Log::notice('getauthCode sucessed . code=' . $authCode);
+                break;
+            }
+            Log::warning('code repetition . id=' . $data_authCode['data'][0]['id']);
+        }
+        EC::success(EC_OK,$authCode);
+    }
+    
+    private function checkCode(){
+        $authCode = Request::post('code');
+        
+        if(!$authCode){
+            Log::error('checkCode params error!');
+            EC::fail(EC_PAR_ERR);
+        }
+        
+        EC::success(EC_OK,"true"); //for test;
+        
+        $code_model = $this->model('authorizationCode');
+        $data_authCode = $code_model->getInfo(array('code' => $authCode));
+        if(EC_OK != $data_authCode['code'] ){
+            Log::error('getInfo failed . code=' . $authCode);
+            EC::fail($data_authCode['code']);
+        }
+        if ( empty($data_authCode['data']) ){
+            Log::notice('getauthCode sucessed . record is empty . code=' . $authCode);
+            EC::success(EC_OK,"false");
+        }
+        
+        $code_data = $data_authCode['data'][0];
+        if(AuthorizationCodeModel::$_is_delete_true == $code_data['is_delete']) {
+            Log::notice('code is delete . code=' . $authCode);
+            EC::success(EC_OK,"delete");
+        }
+        if(AuthorizationCodeModel::$_status_disabled == $code_data['status']) {
+            Log::notice('code is disabled . code=' . $authCode);
+            EC::success(EC_OK,"disabled");
+        } else if(AuthorizationCodeModel::$_status_overdue == $code_data['status']) {
+            Log::notice('code is overdue . code=' . $authCode);
+            EC::success(EC_OK,"overdue");
+        } else if(AuthorizationCodeModel::$_status_enabled == $code_data['status']) {
+            if(self::validataionCodeActive($code_data)){
+                Log::notice('code is check OK . code=' . $authCode);
+                EC::success(EC_OK,"true");
+            } else {
+                Log::notice('code had overdue . code=' . $authCode);
+                EC::success(EC_OK,"overdue");
+            }
+        }
+        Log::error('checkCode . code status is exception . code=' . $authCode . ',status=' . $code_data['status'] );
+        EC::success(EC_OK,"false");
+    }
+    
+    public function validataionCodeActive($code_data){
+        if(empty($code_data)) {
+            return false;
+        }
+        try{
+            $type = $code_data['type'];
+            $type = $code_data['type'];
+            if( AuthorizationCodeModel::$_type_count == $type ){
+                
+            }
+        } catch (Exception $e) {
+            Log::error("validataionCodeActive err . msg=" . $e->getMessage() );
+            return false;
+        }
+        return false;
+    }
+    
+    public static function getCurrentUserId(){
+        $session = Controller::instance('session');
+        $user_id = $session->get('loginUser')['id'];
+        return "1000";
+        return $user_id;
     }
     
 }
--- ddmg_pay/trunk/view/html/authorizationCode.php
+++ ddmg_pay/trunk/view/html/authorizationCode.php
@@ -5,7 +5,7 @@
 ?>
 
 <div class="modal fade" id="add-entity-modal">
-	<div class="modal-dialog" style="width: 1000px;">
+	<div class="modal-dialog" style="width: 700px;">
 		<div class="modal-content">
 			<div class="modal-header">
 				<button type="button" class="close" data-dismiss="modal"
@@ -17,113 +17,45 @@
 			<div class="modal-body">
 				<form class="form-horizontal" id="entity-form">
 				    <input type="hidden" class="form-control" id="info-entity-id" value=""></input>
-				    <input type="hidden" class="form-control" id="info-entity-dm" value=""></input>
 				  <div class="form-group">
-				    <label for="add-entity-tel" class="col-sm-2 control-label">手机号&nbsp;<span style="vertical-align:middle;line-height:2.5;color:red;">*</span></label>
-				    <div class="col-sm-4">
-				      <input class="form-control" id="add-entity-tel" placeholder="手机号"
+				    <label for="add-entity-code" class="col-sm-2 control-label">授权码</label>
+				    <div class="col-sm-6">
+				        <span id="info-entity-code" style="vertical-align:middle;line-height:2.5;">DDMG0099</span>
+				        <input type="hidden" class="form-control" id="add-entity-code" value=""></input>
+				    </div>
+				  </div>
+				  <div class="form-group">
+				    <label for="add-entity-type" class="col-sm-2 control-label">使用方式</label>
+				    <div class="col-sm-1" style="padding-right: 0px;padding-left: 0px;"><input type="radio" class="form-control" id="add-entity-type-count" name="add-entity-type-rdo" value="1" checked="checked"/></div>
+				    <div class="col-sm-2" style="vertical-align:middle;line-height:2.8;padding-left: 0px;">按次数</div>
+				    <div class="col-sm-1" style="padding-right: 0px;padding-left: 0px;"><input type="radio" class="form-control" id="add-entity-type-time" name="add-entity-type-rdo" value="2"/></div>
+				    <div class="col-sm-2" style="vertical-align:middle;line-height:2.8;padding-left: 0px;">按时间</div>
+				  </div>
+				  <div class="form-group" id="div-active-count">
+				    <label for="add-entity-active_count" class="col-sm-2 control-label">可用次数</label>
+				    <div class="col-sm-6">
+				      <input class="form-control" id="add-entity-active_count" placeholder="可用次数"
 				            onkeyup="value=value.replace(/[^\d]/g,'')" onbeforepaste="clipboardData.setData('text',clipboardData.getData('text').replace(/[^\d]/g,''))"></input>
 				    </div>
-				    <label for="add-entity-city" class="col-sm-2 control-label">城市</label>
-				    <div class="col-sm-4">
-				          <select class="form-control" id="add-entity-city"></select>
+				  </div>
+				  <div class="form-group" id="div-time-start" style="display: none;">
+				    <label for="add-entity-time_start" class="col-sm-2 control-label">有效开始时间</label>
+				    <div class="col-sm-6">
+				      <input class="form-control form_datetime" id="add-entity-time_start" placeholder="有效开始时间"></input>
 				    </div>
 				  </div>
-				  <div class="form-group">
-				    <label for="add-entity-name" class="col-sm-2 control-label">性别</label>
-				    <div class="col-sm-4">
-				      <select class="form-control" id="add-entity-gender">
-                	      <option value="0">未定义</option>
-                          <option value="1">男</option>
-                          <option value="2">女</option>
-                      </select>
-				    </div>
-				    <label for="add-entity-info" class="col-sm-2 control-label">签名</label>
-				    <div class="col-sm-4">
-				    	<input class="form-control" rows="3" id="add-entity-info"  placeholder="签名"></input>
-				    </div>
-				  </div>
-				  <div class="form-group">
-				    <label for="add-entity-name" class="col-sm-2 control-label">姓名&nbsp;<span style="vertical-align:middle;line-height:2.5;color:red;">*</span></label>
-				    <div class="col-sm-4">
-				      <input class="form-control" id="add-entity-name" placeholder="姓名"></input>
-				    </div>
-				    <label for="add-entity-company" class="col-sm-2 control-label">公司名称</label>
-				    <div class="col-sm-4">
-				      <input class="form-control" id="add-entity-company" placeholder="公司名称"></input>
-				    </div>
-				  </div>
-				  <div class="form-group">
-				    <label for="add-entity-cardId" class="col-sm-2 control-label">身份证号&nbsp;<span class="span-tips-red">*</span></label>
-				    <div class="col-sm-4">
-				      <input class="form-control" id="add-entity-cardId" placeholder="身份证号"></input>
-				    </div>
-				    <label for="add-entity-bankName" class="col-sm-2 control-label">银行名称</label>
-				    <div class="col-sm-4">
-				      <input class="form-control" id="add-entity-bankName" placeholder="银行名称"></input>
-				    </div>
-				  </div>
-				  <div class="form-group">
-				    <label for="add-entity-bankNum" class="col-sm-2 control-label">银行账号</label>
-				    <div class="col-sm-4">
-				      <input class="form-control" id="add-entity-bankNum" placeholder="银行账号"
-				      onkeyup="value=value.replace(/[^\d]/g,'')" onbeforepaste="clipboardData.setData('text',clipboardData.getData('text').replace(/[^\d]/g,''))"></input>
-				    </div>
-				    <label for="add-entity-bankNumBranch" class="col-sm-2 control-label">开户支行名称</label>
-				    <div class="col-sm-4">
-				      <input class="form-control" id="add-entity-bankNumBranch" placeholder="开户支行名称"></input>
-				    </div>
-				  </div>
-				  <div class="form-group">
-				    <label for="add-entity-advantageProduct" class="col-sm-2 control-label">擅长品种</label>
-				    <div class="col-sm-4">
-				      <input class="form-control" id="add-entity-advantageProduct" placeholder="擅长品种"></input>
+				  <div class="form-group" id="div-time-end" style="display: none;">
+				    <label for="add-entity-time_end" class="col-sm-2 control-label">有效结束时间</label>
+				    <div class="col-sm-6">
+				      <input class="form-control form_datetime" id="add-entity-time_end" placeholder="有效结束时间"></input>
 				    </div>
 				 </div>
 				  <div class="form-group">
-				    <span id="entity-attachment-file" style="display:none;">
-    				    <label for="add-entity-attachment" class="col-sm-2 control-label">协议附件</label>
-    				    <div class="col-sm-4">
-    				      <input type="file" class="form-control" id="file" name="file" placeholder="协议附件"></input>
-    				    </div>
-    				    <div class="col-sm-1" style="vertical-align:middle;line-height:2.5;">
-    				        <a id="entity-attachment-clear" href="#" title="清除选择">
-    				            <span class="glyphicon glyphicon-remove" aria-hidden="true" style="font-size:10px;"></span>
-    				        </a>
-    				    </div>
-    				</span>
-				    <input type="hidden" class="form-control" id="add-entity-attachmentName"/>
-				    <input type="hidden" class="form-control" id="add-entity-attachmentPath"/>
-				    <span id="entity-attachment-download" style="display:none;">
-    				    <label for="add-entity-download" id="add-entity-download-txt" class="col-sm-1 control-label">下载</label>
-    				    <div class="col-sm-3" style="vertical-align:middle;line-height:2.5;">
-    				      <a id="entity-attachment-name" href="#" title="协议附件">[协议附件]</a>
-    				    </div>
-    				    <div class="col-sm-1" style="vertical-align:middle;line-height:2.5;">
-				            <a id="entity-attachment-clearServer" href="#" title="删除协议附件">
-				                <span class="glyphicon glyphicon-remove" aria-hidden="true" style="font-size:10px;"></span>
-				            </a>
-				        </div>
-				    </span>
-				    <input type="hidden" class="form-control" id="add-entity-attachmentDelete"/>
-				  </div>
-				  <div class="form-group">
-				  <span id="entity-attachment-code" style="display:none;">
-				    <label for="add-entity-inviteCode" class="col-sm-2 control-label">邀请码</label>
-				    <div class="col-sm-4">
-				      <input class="form-control" id="add-entity-inviteCode" placeholder="邀请码"></input>
+				    <label for="add-entity-comment" class="col-sm-2 control-label">备注</label>
+				    <div class="col-sm-6">
+				      <input class="form-control" id="add-entity-comment" placeholder="备注"></input>
 				    </div>
 				  </span>
-				 </div>
-				 <div class="form-group">
-				    <label for="add-entity-fgs" class="col-sm-2 control-label">分公司&nbsp;<span class="span-tips-red">*</span></label>
-				    <div class="col-sm-4">
-				      <select class="form-control" id="add-entity-fgs"></select>
-				    </div>
-				    <label for="add-entity-bmdm" class="col-sm-2 control-label">部门&nbsp;<span class="span-tips-red">*</span></label>
-				    <div class="col-sm-4">
-				      <select class="form-control" id="add-entity-bmdm"></select>
-				    </div>
 				 </div>
 				  <div class="alert alert-danger" id="add-entity-hint"></div>
 				</form>
@@ -149,30 +81,32 @@
   <div class="panel-body">
     <div class="form-inline">
 	  <div class="form-group">
-	    <label for="entity-search-tel">手机号</label>
-	    <input type="text" class="form-control" id="entity-search-tel" placeholder="手机号"
-	           onkeyup="value=value.replace(/[^\d]/g,'')" onbeforepaste="clipboardData.setData('text',clipboardData.getData('text').replace(/[^\d]/g,''))">
-	  </div>
-	  <div class="form-group">
 	    <label for="entity-search-code">授权码</label>
-	    <input type="email" class="form-control" id="entity-search-code" placeholder="授权码">
+	    <input type="text" class="form-control" id="entity-search-code" placeholder="授权码">
 	  </div>
-	  <div class="form-group">
-	    <label for="entity-search-company">公司名称</label>
-	    <input type="email" class="form-control" id="entity-search-company" placeholder="公司名称">
-	  </div>
+	  <!-- 
 	  <div class="form-group">
 	    <label for="entity-search-time">添加时间</label>
-	    <input type="email" class="form-control form_datetime" id="entity-search-time1" placeholder="添加时间">
+	    <input type="text" class="form-control form_datetime" id="entity-search-time1" placeholder="添加时间">
 	    -
-	    <input type="email" class="form-control form_datetime" id="entity-search-time2" placeholder="添加时间">
+	    <input type="text" class="form-control form_datetime" id="entity-search-time2" placeholder="添加时间">
+	  </div>
+	   -->
+	  <div class="form-group">
+	    <label for="entity-search-type">使用方式</label>
+	    <select class="form-control" id="entity-search-type">
+	      <option value="-1">全部</option>
+          <option value="1">按次数</option>
+          <option value="2">按时间</option>
+        </select>
 	  </div>
 	  <div class="form-group">
-	    <label for="entity-search-enabledStatus">状态</label>
-	    <select class="form-control" id="entity-search-enabledStatus">
+	    <label for="entity-search-status">状态</label>
+	    <select class="form-control" id="entity-search-status">
 	      <option value="-1">全部</option>
           <option value="1">启用</option>
           <option value="2">停用</option>
+          <option value="3">失效</option>
         </select>
 	  </div>
 	  <button type="button" class="btn btn-primary" id="entity-search-btn">查询</button>
