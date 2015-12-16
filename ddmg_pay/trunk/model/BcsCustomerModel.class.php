d1fde3b01bdada8977dd4592e5471dae7ee8a89f
zhangkui <zhangkui@d4452c07-b0bf-42be-a67f-5257330459db>
2015-12-16 06:17:09 +0000

    客户信息查询 接口。

--- ddmg_pay/trunk/model/BcsCustomerModel.class.php
+++ ddmg_pay/trunk/model/BcsCustomerModel.class.php
@@ -1,7 +1,12 @@
 <?php
 
-class BcsRegisterModel extends CurlModel {
+class BcsCustomerModel extends CurlModel {
    
+    // 客户状态 1-已注册；2-已签约；3-已注销
+    public static $_status_register = 1;
+    public static $_status_signed = 2;
+    public static $_status_cancelled = 3;
+    
     // 导出数据 1-当前页 2-全部
     public static $_export_type_page = 1;
     public static $_export_type_all = 2;
@@ -9,23 +14,23 @@ class BcsRegisterModel extends CurlModel {
     public static $_empyt_time = '0000-00-00 00:00:00';
     
     public function searchCnt($params = array()){
-        return self::sendRequest('bcsRegister/searchCnt', $params);
+        return self::sendRequest('bcsCustomer/searchCnt', $params);
     }
     
     public function searchList($params = array()){
-        return self::sendRequest('bcsRegister/searchList', $params);
+        return self::sendRequest('bcsCustomer/searchList', $params);
     }
     
     public function update($params = array()){
-        return self::sendRequest('bcsRegister/update', $params);
+        return self::sendRequest('bcsCustomer/update', $params);
     }
     
     public function getInfo($params = array()){
-        return self::sendRequest('bcsRegister/getInfo', $params);
+        return self::sendRequest('bcsCustomer/getInfo', $params);
     }
     
     public function create($params = array()){
-        return self::sendRequest('bcsRegister/create', $params);
+        return self::sendRequest('bcsCustomer/create', $params);
     }
     
 }
--- ddmg_pay/trunk/model/BcsRegisterModel.class.php
+++ ddmg_pay/trunk/model/BcsRegisterModel.class.php
@@ -1,12 +1,7 @@
 <?php
 
-class BcsCustomerModel extends CurlModel {
+class BcsRegisterModel extends CurlModel {
    
-    // 客户状态 1-已注册；2-已签约；3-已注销
-    public static $_status_register = 1;
-    public static $_status_signed = 2;
-    public static $_status_cancelled = 3;
-    
     // 导出数据 1-当前页 2-全部
     public static $_export_type_page = 1;
     public static $_export_type_all = 2;
@@ -14,23 +9,23 @@ class BcsCustomerModel extends CurlModel {
     public static $_empyt_time = '0000-00-00 00:00:00';
     
     public function searchCnt($params = array()){
-        return self::sendRequest('bcsCustomer/searchCnt', $params);
+        return self::sendRequest('bcsRegister/searchCnt', $params);
     }
     
     public function searchList($params = array()){
-        return self::sendRequest('bcsCustomer/searchList', $params);
+        return self::sendRequest('bcsRegister/searchList', $params);
     }
     
     public function update($params = array()){
-        return self::sendRequest('bcsCustomer/update', $params);
+        return self::sendRequest('bcsRegister/update', $params);
     }
     
     public function getInfo($params = array()){
-        return self::sendRequest('bcsCustomer/getInfo', $params);
+        return self::sendRequest('bcsRegister/getInfo', $params);
     }
     
     public function create($params = array()){
-        return self::sendRequest('bcsCustomer/create', $params);
+        return self::sendRequest('bcsRegister/create', $params);
     }
     
 }
