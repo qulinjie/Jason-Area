42a9623d0e96f74c7483faf65b5f057c68ca15bf
zhangsong <zhangsong@d4452c07-b0bf-42be-a67f-5257330459db>
2015-12-22 06:02:49 +0000

    添加消息提醒功能

--- ddmg_pay/trunk/js/custom.js
+++ ddmg_pay/trunk/js/custom.js
@@ -130,7 +130,8 @@ $(function () {
 
         function centerModals() {
             $('#loginModal').each(function(i) {
-                var $clone = $(this).clone().css('display', 'block').appendTo('body'); var top = Math.round(($clone.height() - $clone.find('.modal-content').height()) / 3);
+                var $clone = $(this).clone().css('display', 'block').appendTo('body');
+                var top = Math.round(($clone.height() - $clone.find('.modal-content').height()) / 3);
                 top = top > 0 ? top : 0;
                 $clone.remove();
                 $(this).find('.modal-content').css("margin-top", top);
