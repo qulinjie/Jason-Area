596b04a75d81fdf0185d622b4309600cf0f222dc
zhangsong <zhangsong@d4452c07-b0bf-42be-a67f-5257330459db>
2015-12-28 02:26:59 +0000

    测试出入金明细接口

--- ddmg_pay/trunk/controller/BcsCustomerController.class.php
+++ ddmg_pay/trunk/controller/BcsCustomerController.class.php
@@ -561,7 +561,11 @@ class BcsCustomerController extends BaseController {
     }
 
     private function inflow(){
-        $params   = ['MCH_NO' => $this->getConfig('conf')['MCH_NO'],'PAGE_NUMBER' => 1];
+        $params   = [
+            'MCH_NO' => $this->getConfig('conf')['MCH_NO'],
+            'PAGE_NUMBER' => 1,
+            'PAGE_SIZE' => 50
+        ];
         $bcs_data = $this->model('bank')->customerInflowQuery($params);
         var_dump($bcs_data);
         exit;
