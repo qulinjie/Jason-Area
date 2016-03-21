5c52728a371f3e42df55ebf9ab0a4516fd3e40a1
zhangkui <zhangkui@d4452c07-b0bf-42be-a67f-5257330459db>
2016-03-21 01:13:14 +0000

    页面调整。

--- ddmg_pay/trunk/view/html/tradeRecord_list.php
+++ ddmg_pay/trunk/view/html/tradeRecord_list.php
@@ -33,71 +33,42 @@
 <?php }else{?>
 <div class="panel panel-default">
 <div class="panel-heading">列表</div>
-  <div class="panel-body" style="width: 1500px;overflow-x: scroll;">
-		<table class="table table-hover" style="width: 3000px;max-width: 3000px;overflow-x: scroll;">
+  <div class="panel-body">
+		<table class="table table-hover">
 			<thead>
 				<tr>
 					<th style="display:none">id</th>
-					<th>单号</th>
-					<th>时间</th>
-					<th>供应商</th>
-					<th>金额（元）</th>
-					<th>业务员</th>
-					<th>业务员电话</th>
-					<th>公司电话</th>
+					<th>单据日期</th>
+					<th>申请单号</th>
+					<th>单位名称</th>
+					<th>申请金额（元）</th>
+					<th>款项类别</th>
+					<th>业务单号</th>
+					<th>状态</th>
+					<th>机构</th>
 					<th><span id="order-status-show">操作</span></th>
 				</tr>
 			</thead>
 			<tbody>
-<span id="span-trade-order-list">
     <?php foreach ($data_list as $item){?>       
        <tr>
-            <td style="display:none"><?php echo $item['id'];?><input type="hidden" value="<?php echo $item['order_status'];?>"></td>
-            <td><?php echo $item['order_no'];?></td>
-            <td><?php echo $item['order_timestamp'];?></td>
+            <td style="display:none"><?php echo $item['id'];?><input type="hidden" value="<?php echo $item['apply_status'];?>"></td>
+            <td><?php echo date("Y-m-d",strtotime($item['order_timestamp']));?></td>
+            <td><?php echo $item['apply_no'];?></td>
             <td><?php echo $item['seller_name'];?></td>
             <td><?php echo number_format($item['order_bid_amount'],2);?></td>
-            <td><?php echo $item['partner_name']; ?></td>
-            <td><?php echo $item['partner_tel']; ?></td>
-            <td><?php echo $item['partner_company_tel']; ?></td>
-            <?php if($item['order_status']==TradeRecordModel::$_status_waiting){ ?>
-                <td><a id="add-pay-new" href="#" data-toggle="modal" data-keyboard="false" data-backdrop="static">付款</a></td>
-            <?php }else if($item['order_status']==TradeRecordModel::$_status_paid) { ?>
-                <td>已付</td>
-            <?php } else if($item['order_status']==TradeRecordModel::$_status_refuse) { ?>
-                <td>拒付</td>
-            <?php } ?>
+            <td><?php echo $item['amount_type']; ?></td>
+            <td><?php echo $item['order_no'];?></td>
+            <td><?php if(1==$item['apply_status']){ echo '待审批' ;}
+                    else if(2==$item['apply_status']){ echo '审批通过' ;}
+                    else if(3==$item['apply_status']){ echo '审批驳回' ;}
+                    else { echo $item['apply_status'] ;}
+                ?>
+            </td>
+            <td><?php echo $item['comment']; ?></td>
+            <td><a href="#">审批</a></td>
        </tr>
-       <tr>
-       <td colspan="9">
-           <table style="width: 99%;background-color: #e8e8e8;">
-               <tr>
-    				<td>订单号</td>
-                    <td>品名</td>               
-                    <td>材质</td>
-                    <td>规格</td>
-                    <td>交货地</td>
-                    <td>单价（元/ 吨）</td>                
-                    <td>订购量（吨）</td>
-                    <td>订单金额（元）</td>
-               </tr>
-                <?php foreach ($item['list'] as $v){?>
-                <tr>
-                    <td><?php echo $v['order_no'];?></td>
-                    <td><?php echo $v['item_name'];?></td>
-                    <td><?php echo $v['item_type'];?></td>
-                    <td><?php echo $v['item_size'];?></td>
-                    <td><?php echo $v['item_delivery_addr'];?></td>
-                    <td><?php echo number_format($v['item_price'],2);?></td>
-                    <td><?php echo $v['item_weight'];?></td>
-                    <td><?php echo number_format($v['item_amount'],2);?></td>
-                </tr>
-                <?php }?>
-			</table>
-      </td>
-      </tr>
     <?php }?>
-    </span>
 		</tbody>
 		</table>
 		
