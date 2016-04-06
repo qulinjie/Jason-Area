3768e40be3d23449ee03f50e93cc74598b381e7e
liwei <liwei@d4452c07-b0bf-42be-a67f-5257330459db>
2016-04-06 07:47:33 +0000

    支付密码重置

--- ddmg_pay/trunk/view/html/payPasswordReset.php
+++ ddmg_pay/trunk/view/html/payPasswordReset.php
@@ -1,20 +1,46 @@
-<div class="reset">
-    <form>
-        <input type="hidden" name="token" id="token" value="<?php echo UserController::getToken(); ?>"/>
-        <?php if ($status) { ?>
-            <p class="service_l">当前支付密码<input type="password" name="payOldPwd" id="payOldPwd"></p>
-            <b id="payOldPwdMsg" class="paw"></b>
-            <p class="service_l">新支付密码<input type="password" name="payNewPwd" id="payNewPwd"></p>
-            <b id="payNewPwdMsg" class="pawo"></b>
-            <p class="service_l">确认新支付密码<input type="password" name="payNewPwd2" id="payNewPwd2"></p>
-            <b id="payNewPwd2Msg" class="pawod"></b>
-        <?php } else { ?>
-            <p class="service_l">支付密码<input type="password" name="initPayNewPwd" id="initPayNewPwd"></p>
-            <b id="initPayNewPwdMsg" class="z_paw"></b>
-            <p class="service_l">确认支付密码<input type="password" name="initPayNewPwd2" id="initPayNewPwd2"></p>
-            <b id="initPayNewPwd2Msg" class="z_pawd"></b>
-        <?php } ?>
-        <p class="service_b"><input type="button" id="payPasswordReset" value="确认"></p>
-    </form>
-</div>
 
+<h1 class="page-header">重置支付密码</h1>
+<div class="panel panel-primary">
+  <div class="panel-body">	
+    <div class="form-horizontal">
+       <input type="hidden" id="token" name="token" value="<?php echo UserController::getToken(); ?>"/>
+       <div class="form-group"></div> 
+       
+       <div class="form-group">
+	    <label for="add-entity-apply_no" class="col-sm-2 control-label">新支付密码</label>
+	    <div class="col-sm-3">
+		    <input type="password" class="form-control" id="password" />   
+		    <span id="passwordMsg" style="color:red;"></span> 				   
+	    </div>	    	
+	   </div>
+	  <div class="form-group">
+	    <label for="add-entity-comp_name" class="col-sm-2 control-label">确认新支付密码</label>
+	    <div class="col-sm-3">
+		    <input type="password" class="form-control" id="rePassword" />  
+		    <span id="rePasswordMsg" style="color:red;"></span>  				   
+	    </div>	    
+	  </div>	  
+	  <div class="form-group">
+	    <label for="add-entity-comp_name" class="col-sm-2 control-label">接收短信手机</label>
+	    <div class="col-sm-3">
+		   	<span><?php if(!empty($mobile)){ echo substr_replace($mobile, '****', 3, 4); }?></span>
+	    </div>	    
+	  </div>
+	  <div class="form-group">
+	    <label for="add-entity-apply_no" class="col-sm-2 control-label">短信验证</label>
+	    <div class="col-sm-3">
+		    <input type="text" class="form-control" id="code" name="code" style="width:120px; display: inline;" placeholder="验证码"/>
+            <input type="hidden" id="mobile" name="mobile" value="<?php echo $mobile;?>" />
+            <input type="button" id="sendCode" value="获取验证码" class="btn btn-default" style="background-color: rgb(255,0,255);"/>
+			<span class="help-block" id="codeMsg" style="color:red;"></span> 		
+		</div>	    	
+	   </div>  
+      <div class="alert alert-danger" id="entity-hint" style="display: none;"></div>  
+      <div class="form-group"></div>
+	  <div id="button-group" style="padding-bottom: 2px; margin-left: 30%;">
+    	<a id="pay-pwd-reset" class="btn btn-primary" href="#" style=" width:120px;">确定</a>
+      </div>
+      
+	</div>
+  </div>
+</div>
