96c3c65793ba3c7f535f0758962b70469eea876a
liwei <liwei@d4452c07-b0bf-42be-a67f-5257330459db>
2016-04-08 09:06:19 +0000

    api接口支持admin和user登录

--- ddmg_pay/trunk/controller/ApiController.class.php
+++ ddmg_pay/trunk/controller/ApiController.class.php
@@ -1,7 +1,8 @@
 <?php
 
 class ApiController extends Controller {
-	public static $_apiUserSessionKey = "apiLoginUser";
+	//public static $_apiUserSessionKey = "apiLoginUser";
+	public static $_isAdmin = false;
 	
 	public function handle($params = array()) {}
 	
@@ -14,8 +15,7 @@ class ApiController extends Controller {
 	}
 	
 	//api接口调用安全验证
-	public static function securityValidate(){
-	
+	public static function securityValidate(){	
 	}
 	
 	//获取get/post提交的loginkey
@@ -75,23 +75,35 @@ class ApiController extends Controller {
 			return self::$_isLogin = false;
 		}
 		return self::$_isLogin;
-	}
+	} 
 	
 	//对api方式访问的进行loginkey方式登录user
 	public static function loginByLoginkey(){		
 		$loginkey = self::getLoginkey();
 		if(self::isApi() && !empty($loginkey) && !self::isLogin()){
 			//echo "<br>---- loginByLoginkey ----<br>";			
+			log::notice("---api---loginByLoginkey" . $loginkey);
+			
 			$data = self::model('user')->erp_login_by_loginkey(['loginkey' => $loginkey]);
 			$data['code'] !== EC_OK_ERP && EC::fail($data['code'], $data['msg']);
 			
-			self::setLoginSession($data['data']);			
+			$loginUser = $data['data'];	
+			//如果是admin用户则设置AdminController的session		
+			if(is_array($loginUser) && isset($loginUser['is_admin']) && 'yes' == $loginUser['is_admin']){
+				self::$_isAdmin = true;
+				AdminController::setLoginSession($loginUser);
+			}else{
+				//如果是前台用户则设置UserController的session
+				self::$_isAdmin = false;
+				UserController::setLoginSession($loginUser);
+			}
+			
 			self::$_loginUser = NULL;
 			self::$_isLogin = NULL;
 		}		
 	}
 		
-	protected static function setLoginSession($loginUser){
+	/* protected static function setLoginSession($loginUser){
     	if(empty($loginUser)){
     		Log::error('setLoginSession [loginUser] is empty .');
     		return false;
@@ -99,8 +111,9 @@ class ApiController extends Controller {
     	$session = self::instance('session');    	
     	if(isset($loginUser['password'])) unset( $loginUser['password'] );
     	$session->set(self::$_apiUserSessionKey, $loginUser);    	
+    	
     	return true;
-    }
+    } */
 	
     private static $_loginUser = NULL;
     public static function getLoginUser(){
@@ -108,14 +121,17 @@ class ApiController extends Controller {
     	
     	if(NULL !== self::$_loginUser){
     		return self::$_loginUser;
-    	}    	 
-    	$session = self::instance('session');
-    	if($session->is_set(self::$_apiUserSessionKey)){
-    		$loginUser = $session->get(self::$_apiUserSessionKey);
-    	}    
+    	}
+
+    	if(UserController::isLogin()){
+    		$loginUser = UserController::getLoginUser();
+    	}elseif(AdminController::isAdmin()){
+    		$loginUser = AdminController::getLoginUser();
+    	}    	
     	if(!$loginUser){    		
     		$loginUser = [];    		
     	}    	 
     	return self::$_loginUser = $loginUser;
-    }
+    } 
+	
 }
